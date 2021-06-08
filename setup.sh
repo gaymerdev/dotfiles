b8e7a71ed84f70af09368dd412fd5edb7c84b628
Jonas McClure <jonmcclu@developernexus.app>
2021-06-08 12:55:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ fi
 
 if [[ -f $HOME/.zshrc ]]
 then
-    rm $HOME/.zshrc
+    rm $HOME/.zshrc && \
     ln -s $HOME/profile-configs/zsh/.zshrc $HOME/.zshrc
 fi
 
 if [[ -f $HOME/.p10k.zsh ]]
 then
-    rm $HOME/.p10k.zsh
+    rm $HOME/.p10k.zsh && \
     ln -s $HOME/profile-configs/zsh/.p10k.zsh $HOME/.p10k.zsh
 fi
