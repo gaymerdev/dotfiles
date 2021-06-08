65e454347a320984b4b0e675954589dbe703dca6
Jonas McClure <jonmcclu@developernexus.app>
2021-06-08 05:16:18 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
-curl -L git.io/antigen > antigen.zsh
-git clone https://github.com/zsh-users/antigen.git ~/antigen
+curl -L git.io/antigen > $HOME/antigen.zsh
+git clone https://github.com/zsh-users/antigen.git $HOME/antigen
 
 
 rm $HOME/.zshrc && rm $HOME/.p10k.zsh
