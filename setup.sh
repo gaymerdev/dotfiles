f3686fdef0810626b5ee3f0eea368f929c8d3615
Jonas McClure <jonas@gaymerfamily.com>
2022-05-28 05:50:38 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ if [[ ! -f $HOME/.antigen.zsh ]]; then curl -L git.io/antigen > $HOME/.antigen.z
 
 ## Symnbolically link profile configs from the repository to their expected locations.
 ## Create backups of the file if one aleady exists.
-if [[ -d $HOME/.configs/profile ]]
+if [[ -d $HOME/.config/profile ]]
 then
-    ln --symbolic --backup  $HOME/.configs/profile/zsh/.zshrc $HOME/.zshrc
-    ln --symbolic --backup  $HOME/.configs/profile/zsh/.p10k.zsh $HOME/.p10k.zsh
-    ln --symbolic --backup  $HOME/.configs/profile/ssh/config $HOME/.ssh/config
-    ln --symbolic --backup  $HOME/.configs/profile/.digrc $HOME/.digrc
-    ln --symbolic --backup  $HOME/.configs/profile/.vimrc $HOME/.vimrc
+    ln --symbolic --backup  $HOME/.config/profile/zsh/.zshrc $HOME/.zshrc
+    ln --symbolic --backup  $HOME/.config/profile/zsh/.p10k.zsh $HOME/.p10k.zsh
+    ln --symbolic --backup  $HOME/.config/profile/ssh/config $HOME/.ssh/config
+    ln --symbolic --backup  $HOME/.config/profile/.digrc $HOME/.digrc
+    ln --symbolic --backup  $HOME/.config/profile/.vimrc $HOME/.vimrc
 else
-    echo "The repository needs to be @ $HOME/.configs/profile."
+    echo "The repository needs to be @ $HOME/.config/profile."
 fi
 
 #### Switch to ZSH Shell
-if echo $0 | grep -qiE "zsh"; then echo "Already using ZSH"; else chsh --shell=/usr/bin/zsh $USER; fi
+chsh --shell=/usr/bin/zsh $USER
