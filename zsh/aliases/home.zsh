d82d15f96ef1a82f71ccf1d7cde093424daf6203
Jonas McClure <jonmcclu@developernexus.app>
2022-10-31 22:54:56 -0400

    Updating alias file

--- zsh/aliases/home.zsh
+++ zsh/aliases/home.zsh
@@ -7,7 +7,6 @@ alias auri='yay -Sy --nodiffmenu --save'
 alias auru='yay -Rs'
 alias paci='sudo pacman -Sy --noconfirm --needed'
 alias pacu='sudo pacman -Rs'
-alias rcp='rsync -avh --progress'
 
 function upgrade {
     sudo pacman -Syyu --noconfirm;
@@ -16,3 +15,10 @@ function upgrade {
 
 alias upgrade='upgrade'
 # <<< Package Management
+
+# >>> Command Shortcuts
+
+alias code='/usr/bin/code-insiders'
+alias rcp='rsync -avh --progress'
+
+# <<< Command Shortcuts
