932b039edd02f69cae58c5ac8aec72b2e447d179
Jonas McClure <jonas@gaymerfamily.com>
2023-03-02 11:56:51 +0000

    Put autoinstall of vim-plug into vimrc

--- setup.sh
+++ setup.sh
@@ -16,12 +16,5 @@ else
     echo "The repository needs to be @ $HOME/.config/profile."
 fi
 
-## Install Vim-Plug if the directory does not yet exist.
-if [[ ! -d $HOME/.vim/plugged ]]
-then
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-fi
-
 #### Switch to ZSH Shell
 chsh --shell=/usr/bin/zsh $USER
